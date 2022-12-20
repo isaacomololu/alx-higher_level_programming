148ee9cb413c7a2e7898ccacf305f09c76cd809b
Isaac Omololu <isaaccassij.omo@gmail.com>
2022-12-20 01:04:59 -0800

    One

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,12 @@ def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            print(my_list[x])
+            print(f"{my_list[i]}", end="")
             total += 1
-            return total
+        
         except IndexError:
             print("Index is out of bounds")
+
+    print()
+    return(total)
 
