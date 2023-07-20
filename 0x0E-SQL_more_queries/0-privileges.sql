f37af0f1af29efe85ff3928e8c1765dedd8e87ba
isaacomololu <isaaccassij.omo@gmail.com>
2023-07-20 16:20:16 -0700

    chmod upd

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
--- A script that lists all privileges of the MySQL users
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
