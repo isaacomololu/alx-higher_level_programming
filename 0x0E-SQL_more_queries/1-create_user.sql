0610ed56ace3b2b0c82d7602cbf95d972a833059
isaacomololu <isaaccassij.omo@gmail.com>
2023-07-20 15:28:28 -0700

    1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+A script that lists all privileges of the MySQL users
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
