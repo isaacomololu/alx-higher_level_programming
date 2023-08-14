76c18fa9a107a14849a93da7d8e367e4e5c95abb
isaacomololu <isaaccassij.omo@gmail.com>
2023-08-14 22:25:35 +0000

    task?

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+if (process.argv[2] && process.argv[3]) {
+    console.log(process.argv[2] + ' is ' + process.argv[3]);
+  } else {
+    console.log('Please provide two arguments.');
+  }
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+const parsedInt = parseInt(process.argv[2]);
+
+if (!isNaN(parsedInt)) {
+  console.log("My number:", parsedInt);
+} else {
+  console.log("Not a number");
+}
